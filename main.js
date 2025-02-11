7d44118cf832f1e8f4109ff1ec60e08a2571924e
Kristian Pejić <108726003+KristianPejic@users.noreply.github.com>
2025-02-11 07:39:08 +0100

    Animations

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 function toggleMenu() {
   document.querySelector(".nav-links").classList.toggle("active");
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const text = "Dobrodošli u Mostar";
+  let index = 0;
+  const speed = 100;
+
+  function typeWriter() {
+    if (index < text.length) {
+      document.getElementById("typewriter").innerHTML += text.charAt(index);
+      index++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+  typeWriter();
+});
